07bc3ed4fcdebbe26398cba6dd6cb32f4965b907
Dovile <dovile.tol@gmail.com>
2017-04-05 23:28:48 +0300

    pridėtas slaptažodžių, vartotojo vardo ir elpašto tikrinimas sukūrus įrašą, vaizduojami jo duomenys Validacija

--- app/src/main/java/com/example/dovile/foxy/NewPost.java
+++ app/src/main/java/com/example/dovile/foxy/NewPost.java
@@ -1,50 +1,124 @@
 package com.example.dovile.foxy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
-import android.widget.Spinner;
-import android.widget.AdapterView.OnItemSelectedListener;
-import java.util.ArrayList;
-import java.util.List;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-import android.app.Activity;
-import android.view.View.OnClickListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.widget.Spinner;
 
 public class NewPost extends SecondActivity {
-
-    private Spinner spinner1;
+    private EditText mAmmountView;
+    private EditText mPriceView;
+    private EditText mLengthView;
+    private EditText mWidthView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_post);
+        Button saugoti = (Button) findViewById(R.id.save);
+
+        mAmmountView = (EditText) findViewById(R.id.kiekis);
+        mPriceView = (EditText) findViewById(R.id.kaina);
+        mLengthView = (EditText) findViewById(R.id.ilgis);
+        mWidthView = (EditText) findViewById(R.id.plotis);
+
+        saugoti.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mAmmountView.setError(null);
+                mPriceView.setError(null);
+                mLengthView.setError(null);
+                mWidthView.setError(null);
+
+                String ammount = mAmmountView.getText().toString();
+                String price = mPriceView.getText().toString();
+                String length = mLengthView.getText().toString();
+                String width = mWidthView.getText().toString();
+
+                boolean cancel = false;
+                View focusView = null;
+
+                if (!isValid(ammount)) {
+                    mAmmountView.setError("Neteisingai nurodytas kiekis.");
+                    focusView = mAmmountView;
+                    cancel = true;
+                }
+                if (!isValid2(price)) {
+                    mPriceView.setError("Neteisingai nurodyta kaina.");
+                    focusView = mPriceView;
+                    cancel = true;
+                }
+                if (!isValid2(length)) {
+                    mLengthView.setError("Neteisingai nurodytas ilgis.");
+                    focusView = mLengthView;
+                    cancel = true;
+                }
+                if (!isValid2(width)) {
+                    mWidthView.setError("Neteisingai nurodytas plotis.");
+                    focusView = mWidthView;
+                    cancel = true;
+                }
+
+               final Spinner tipas = (Spinner) findViewById(R.id.tipas);
+                final EditText kiekisEditText = (EditText) findViewById(R.id.kiekis);
+                final Spinner uzsakymai = (Spinner) findViewById(R.id.uzsakymai);
+                final EditText kainaEditText = (EditText) findViewById(R.id.kaina);
+                final Spinner technika = (Spinner) findViewById(R.id.technika);
+                final   EditText ilgisEditText = (EditText) findViewById(R.id.ilgis);
+                final  EditText plotisEditText = (EditText) findViewById(R.id.plotis);
+                final Spinner reguliavimas = (Spinner) findViewById(R.id.reguliavimas);
+
+                //@Override
+
+                //public void onClick (View v)
+                        //{
+                        String sKiekis = kiekisEditText.getText().toString();
+                String sKaina = kainaEditText.getText().toString();
+                String sIlgis = ilgisEditText.getText().toString();
+                String sPlotis = plotisEditText.getText().toString();
+                if (sKiekis.matches("") || sKaina.matches("") || sIlgis.matches("") || sPlotis.matches("")) {
+                    Toast.makeText(NewPost.this, "Patikrinkite, ar įvedėte visus duomenis.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Toast.makeText(NewPost.this,
+                        String.valueOf(tipas.getSelectedItem()) + "\n" +
+                                kiekisEditText.getText() + "\n" +
+                                String.valueOf(uzsakymai.getSelectedItem()) + "\n" +
+                                kainaEditText.getText() + "\n" +
+                                String.valueOf(technika.getSelectedItem()) + "\n" +
+                                ilgisEditText.getText() + "\n" +
+                                plotisEditText.getText() + "\n" +
+                                String.valueOf(reguliavimas.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+                if (cancel) {
+                    focusView.requestFocus();
+                }
+                else {
+                    Intent intent = new Intent(NewPost.this, NewPost.class);
+                    startActivity(intent);
+                }
+            }
+
+        });
     }
 
-    public void CreatePost (View v){
-
-        EditText kiekisEditText = (EditText) findViewById(R.id.kiekis);
-        String sKiekis = kiekisEditText.getText().toString();
-        EditText kainaEditText = (EditText) findViewById(R.id.kaina);
-        String sKaina = kainaEditText.getText().toString();
-        EditText ilgisEditText = (EditText) findViewById(R.id.ilgis);
-        String sIlgis = ilgisEditText.getText().toString();
-        EditText plotisEditText = (EditText) findViewById(R.id.plotis);
-        String sPlotis = plotisEditText.getText().toString();
-        if (sKiekis.matches("") || sKaina.matches("") || sIlgis.matches("") || sPlotis.matches("")) {
-            Toast.makeText(this, "Patikrinkite, ar įvedėte visus duomenis.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Intent intent = new Intent (NewPost.this, NewPost.class);
-        startActivity(intent);
-        Toast.makeText(this, "Įrašas įtrauktas sėkmingai!", Toast.LENGTH_SHORT).show();
+    private boolean isValid(String credentials) {
+        final String CREDENTIALS_PATTERN = "^([0-9])+$";
+        Pattern pattern = Pattern.compile(CREDENTIALS_PATTERN);
+        Matcher matcher = pattern.matcher(credentials);
+        return matcher.matches();
     }
 
+    private boolean isValid2(String credentials) {
+        final String CREDENTIALS_PATTERN2 = "^([0-9]+.+[0-9])+$";
+        Pattern pattern2 = Pattern.compile(CREDENTIALS_PATTERN2);
+        Matcher matcher = pattern2.matcher(credentials);
+        return matcher.matches();
+    }
 }
--- app/src/main/java/com/example/dovile/foxy/SignUp.java
+++ app/src/main/java/com/example/dovile/foxy/SignUp.java
@@ -4,39 +4,97 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SignUp extends MainActivity {
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private EditText mPassword2View;
+    private EditText mEmailView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        Button registruotis = (Button) findViewById(R.id.button3);
+
+        mUsernameView = (EditText) findViewById(R.id.Vart);
+        mPasswordView = (EditText) findViewById(R.id.Slapt1);
+        mPassword2View = (EditText) findViewById(R.id.Slapt2);
+        mEmailView = (EditText) findViewById(R.id.Elp);
+
+        registruotis.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mUsernameView.setError(null);
+                mPasswordView.setError(null);
+                mPassword2View.setError(null);
+                mEmailView.setError(null);
+                String username = mUsernameView.getText().toString();
+                String password = mPasswordView.getText().toString();
+                String email = mEmailView.getText().toString();
+                EditText password2EditText = (EditText) findViewById(R.id.Slapt2);
+                String password2 = password2EditText.getText().toString();
+
+                boolean cancel = false;
+                View focusView = null;
+
+                if (!isValid(username)) {
+                    mUsernameView.setError("Neteisingas vartotojo vardas.");
+                    focusView = mPasswordView;
+                    cancel = true;
+                }
+                if (!isValid(password)) {
+                    mPasswordView.setError("Neteisingas slaptažodis.");
+                    focusView = mPasswordView;
+                    cancel = true;
+                }
+                if (!isValid2(email)) {
+                    mEmailView.setError("Neteisingas el.paštas.");
+                    focusView = mEmailView;
+                    cancel = true;
+                }
+                if (password2.matches("")) {
+                    mPassword2View.setError("Pakartokite slaptažodį.");
+                    focusView = mPassword2View;
+                    cancel = true;
+                }
+                if (password.matches(username)) {
+                    mPasswordView.setError("Prisijungimo vardas ir slaptažodis negali sutapti.");
+                    focusView = mPasswordView;
+                    cancel = true;
+                }
+                if (!password.matches(password2)) {
+                    mPassword2View.setError("Slaptažodžiai nesutampa.");
+                    focusView = mPassword2View;
+                    cancel = true;
+                }
+                if (cancel) {
+                    focusView.requestFocus();
+                }
+                else {
+                    Intent intent = new Intent(SignUp.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
-   public void signup3 (View v){
-
-       EditText usernameEditText = (EditText) findViewById(R.id.Vart);
-       String sVart = usernameEditText.getText().toString();
-       EditText password1EditText = (EditText) findViewById(R.id.Slapt1);
-       String sSlapt1 = password1EditText.getText().toString();
-       EditText password2EditText = (EditText) findViewById(R.id.Slapt2);
-       String sSlapt2 = password2EditText.getText().toString();
-       EditText emailEditText = (EditText) findViewById(R.id.Elp);
-        String sElp = password2EditText.getText().toString();
-        if (sVart.matches("") || sSlapt1.matches("") || sSlapt2.matches("") || sElp.matches("") ) {
-            Toast.makeText(this, "Neįvedėte duomenų", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (sSlapt1.matches(sSlapt2)) {
-            Toast.makeText(this, "Registracija sėkminga!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent (SignUp.this, MainActivity.class);
-            startActivity(intent);
-        }
-       else Toast.makeText(this, "Slaptažodžiai nesutampa.", Toast.LENGTH_SHORT).show();
-        return;
-
+    private boolean isValid(String credentials){
+        final String CREDENTIALS_PATTERN ="^([0-9a-zA-Z]{3,15})+$";
+        Pattern pattern =Pattern.compile(CREDENTIALS_PATTERN);
+        Matcher matcher = pattern.matcher(credentials);
+        return matcher.matches();
+    }
+    private boolean isValid2(String credentials){
+        final String CREDENTIALS_PATTERN2 ="^([0-9a-zA-Z]+@+[a-z]+.+[a-z])+$";
+        Pattern pattern2 =Pattern.compile(CREDENTIALS_PATTERN2);
+        Matcher matcher = pattern2.matcher(credentials);
+        return matcher.matches();
     }
 
 }
